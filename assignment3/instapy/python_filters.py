08079f6891be4e634b15a7da597cf8bd5dbb094e
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-09-15 10:28:57 +0200

    Start implementation of grayscale
    More like greenscale amirite guys

--- assignment3/instapy/python_filters.py
+++ assignment3/instapy/python_filters.py
@@ -11,11 +11,24 @@ def python_color2gray(image: np.array) -> np.array:
     Returns:
         np.array: gray_image
     """
+    pixels = np.asarray(image)
     gray_image = np.empty_like(image)
+
+    m, n, p = pixels.shape
+    gray_transform = [0.21, 0.72, 0.07]
+
+    for i in range(m):
+        for j in range(n):
+            for k in range(p):
+                gray_image[i][j][k] = pixels[i][j][k]*gray_transform[k]
+
+    print(pixels[0,0])
+    print(gray_image[0,0])
+
     # iterate through the pixels, and apply the grayscale transform
 
-    ...
-    return gray_image
+    # return pixels
+    return gray_image.astype("uint8")
 
 
 def python_color2sepia(image: np.array) -> np.array:
