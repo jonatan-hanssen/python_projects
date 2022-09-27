Fix overflow handling

assignment3/instapy/numba_filters.py
@@ -73,18 +73,22 @@ def numba_color2sepia(image: np.array, k: Optional[float] = 1) -> np.array:
         for j in range(3):
             sepia_matrix[i][j] += scaling_matrix[i][j]*(1-k)
 
+    highest = 0
     for i in range(m):
         for j in range(n):
             for k in range(p):
                 sepia_image[i][j][k] = image[i][j][0]*sepia_matrix[k][0]
                 sepia_image[i][j][k] += image[i][j][1]*sepia_matrix[k][1]
                 sepia_image[i][j][k] += image[i][j][2]*sepia_matrix[k][2]
-            # fix overflows
-            highest = max(sepia_image[i][j])
-            if highest > 255:
-                ratio = 255/highest
-                sepia_image[i][j][0] *= ratio
-                sepia_image[i][j][1] *= ratio
-                sepia_image[i][j][2] *= ratio
+            highest = max(max(sepia_image[i][j]), highest)
+
+
+    # fix overflows
+    if highest > 255:
+        ratio = 255/highest
+        for i in range(m):
+            for j in range(n):
+                for k in range(p):
+                    sepia_image[i][j][k] *= ratio
 
     return sepia_image.astype("uint8")

assignment3/instapy/numpy_filters.py
@@ -80,15 +80,6 @@ def numpy_color2sepia(image: np.array, k: Optional[float] = 1) -> np.array:
     sepia_image = np.einsum('ijk,lk->ijl',image, sepia_matrix)
 
     # fix overflows
-
-    # find the highest value along the third axis (the rgb values)
-    maxes = sepia_image.max(2) # this returns a M,N array
-
-    overflows = np.where(maxes > 255, maxes, 255) # an array of 255 or higher values
-    ratios = (np.ones((m,n))*255)/overflows # this is either one or something lower
-
-    # this multiplies every pixel value with either one or the appropriate ratio in case
-    # of overflows
-    sepia_image *= np.dstack((ratios,ratios,ratios))
+    sepia_image *= 255/(sepia_image.max())
 
     return sepia_image.astype("uint8")