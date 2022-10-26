b1f83f0883aeecd977f620d58bd48dbbd2654e8e
jonatan-hanssen <jonatan.hanssen@outlook.com>
2022-10-26 21:10:41 +0200

    Add a very inefficient "wiki race" entry

--- assignment4/wiki_race_challenge.py
+++ assignment4/wiki_race_challenge.py
@@ -1,4 +1,7 @@
 from typing import List  # isort:skip
+from requesting_urls import get_html
+from filter_urls import find_articles
+import re
 
 
 def find_path(start: str, finish: str) -> List[str]:
@@ -16,8 +19,55 @@ def find_path(start: str, finish: str) -> List[str]:
         All items of the list should be URLs for wikipedia articles.
         Each article should have a direct link to the next article in the list.
     """
-    path = ...
-    ...
+
+    # we will use BFS, as this will give us the shortest path
+
+    class Node:
+        """tiny class for storing previous urls so that we can trace back the path
+        parameters:
+            url (string): an url to a wikipedia article
+            parent (Node): the node of the url which led us to the url in this node
+        """
+
+        def __init__(self, url, parent):
+            self.url = url
+            self.parent = parent
+            if parent == None:
+                self.layer = 0
+            else:
+                self.layer = parent.layer + 1
+
+    queue = [Node(start, None)]
+
+    while queue:
+        node = queue.pop(0)
+        only_article_pattern = r"\/wiki\/(.*)"
+        match = re.search(only_article_pattern, node.url)
+        only_article = match.group(1)
+        print(
+            f"Articles found: {len(queue)} | Layer: {node.layer} | Article: {only_article}"
+        )
+        if node.url == finish:
+            break
+        html = get_html(node.url)
+        articles = find_articles(html)
+        node_list = [Node(url, node) for url in articles]
+        queue.extend(node_list)
+        if len(queue) == 0:
+            print("No path found")
+            exit()
+
+    path = list()
+
+    # trace back to start
+    while node.parent:
+        path.insert(0, node.url)
+        node = node.parent
+
+    # we broke the while loop while on the first node, so we must append its
+    # url aswell to get the start
+    path.insert(0, node.url)
+
     assert path[0] == start
     assert path[-1] == finish
     return path
@@ -26,4 +76,6 @@ def find_path(start: str, finish: str) -> List[str]:
 if __name__ == "__main__":
     start = "https://en.wikipedia.org/wiki/Python_(programming_language)"
     finish = "https://en.wikipedia.org/wiki/Peace"
-    find_path(start, finish)
+
+    path = find_path(start, finish)
+    print(path)
